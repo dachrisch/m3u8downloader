6ff68259d37de6ff7237129e2a44b118bcc17917
Christian Dähn <christian.daehn@gmail.com>
2022-03-05 22:11:54 +0100

    retrieve from fetchted urls

--- ex/downloader.py
+++ ex/downloader.py
@@ -24,43 +24,78 @@ class VideoFile:
         return f'{self.__class__.__name__}(name={self.filename}, url={self.url})'
 
 
-class DropboxM3u8Retriever:
-    def __init__(self):
-        self.videos: List[VideoFile] = []
-        self.browser = WebDriver(service=Service('./chromedriver'))
-        self.browser.implicitly_wait(10)
-        self.wait = WebDriverWait(self.browser, timeout=3)
+class DropboxVideoPage:
+    def __init__(self, browser, video_url):
+        self.video_url = video_url
+        self.browser = browser
         self.log = getLogger(self.__class__.__name__).info
 
-    def get_video_files(self, url, password):
-        self.login(url, password)
-        for video_url in self.collect_video_urls():
-            self.load_video_player(video_url)
+    def __enter__(self):
+        self.browser.get(self.video_url)
+        source = self.browser.find_element(By.XPATH, '//source')
+        self.log(f'obtained video [{self.browser.title}] at [{source.get_attribute("src")}]')
+        return VideoFile(self.browser.title, source.get_attribute('src'))
 
-        self.browser.quit()
-        return self.videos
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class DropboxVideoListPage:
+    def __init__(self, browser):
+        self.browser = browser
+        self.wait = WebDriverWait(self.browser, timeout=3)
+
+    def get_video_files(self):
+        videos: List[VideoFile] = []
+        for video_page in self.collect_video_urls():
+            with video_page as video_file:
+                videos.append(video_file)
+
+        return videos
 
     def collect_video_urls(self):
         self.wait.until(item_present)
         video_links = self.browser.find_elements(By.XPATH, '//td/a')
-        video_urls = list(map(lambda video: video.get_attribute('href'), video_links))
+        video_urls = list(map(lambda video: DropboxVideoPage(self.browser, video.get_attribute('href')), video_links))
         return video_urls
 
-    def login(self, url, password):
+
+class DropboxLoginPage:
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+        self.log = getLogger(self.__class__.__name__).info
+        self.wait = WebDriverWait(self.browser, timeout=3)
+
+    def __enter__(self):
+        self.browser.get(self.url)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.browser.quit()
+
+    def login(self, password):
+        self._login(self.url, password)
+        return DropboxVideoListPage(self.browser)
+
+    def no_login(self):
+        self.log(f'calling page at: {self.url}')
+        return DropboxVideoListPage(self.browser)
+
+    def _login(self, url, password):
         self.log(f'logging into page at: {url}')
-        self.browser.get(url)
         self.wait.until(button_enabled)
         self.browser.find_element(By.NAME, 'shared-link-password').send_keys(password)
         self.browser.find_element(By.CLASS_NAME, 'mc-button-content').click()
 
-    def load_video_player(self, video_url):
-        self.browser.get(video_url)
-        source = self.browser.find_element(By.XPATH, '//source')
-        self.log(f'obtained video [{self.browser.title}] at [{source.get_attribute("src")}]')
-        self.found_video(self.browser.title, source.get_attribute('src'))
 
-    def found_video(self, title, url):
-        self.videos.append(VideoFile(title, url))
+class DropboxM3u8Retriever:
+    def __init__(self):
+        self.browser = WebDriver(service=Service('./chromedriver'))
+        self.browser.implicitly_wait(10)
+
+    def load(self, url) -> DropboxLoginPage:
+        return DropboxLoginPage(self.browser, url)
 
 
 def main():
@@ -74,12 +109,18 @@ def main():
     with open(args.url_file, 'r') as urls_file:
         video_json = json.load(urls_file)
 
-    video_files = DropboxM3u8Retriever().get_video_files(video_json['url'], video_json['password'])
+    with DropboxM3u8Retriever().load(video_json['url']) as page:
+        if 'password' in video_json:
+            video_page = page.login(video_json['password'])
+        else:
+            video_page = page.no_login()
+
+        video_files = video_page.get_video_files()
 
     for video_file in video_files:
         filename = f'{args.directory}/{video_file.filename}'
         if os.path.isfile(filename):
-            log(f'skipping already present {video_file}')
+            log(f'skipping already present [{video_file.filename}]')
         else:
             downloader = M3u8Downloader(video_file.url, filename, tempdir=args.directory)
             log(f'downloading {video_file}...')
